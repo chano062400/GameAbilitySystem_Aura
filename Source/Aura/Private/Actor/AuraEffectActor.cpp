9d15c2edff85de547c17b7e76c9844ef26807816
chano062400 <sch062400@gmail.com>
2023-10-26 03:08:12 +0900

    Project Aura

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -1,48 +1,43 @@
 #include "Actor/AuraEffectActor.h"
-#include "Components/SphereComponent.h"
-#include "Components/StaticMeshComponent.h"
 #include "AbilitySystemInterface.h"
-#include "AbilitySystem/AuraAbilitySystemComponent.h"
-#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 
 AAuraEffectActor::AAuraEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(RootComponent);
-}
-
-void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (IAbilitySystemInterface* Interface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		//Gameplay Effect를 아직 배우지 않아서 억지로 const_cast후 값을 변경.
-		if (const UAuraAttributeSet* AuraAttributeSet = Cast<UAuraAttributeSet>(Interface->GetAbilitySystemComponent()->GetAttributeSet(UAttributeSet::StaticClass()))) // const값이라 원래는 변경 못함.
-		{
-			UAuraAttributeSet* MutableAAS= const_cast<UAuraAttributeSet*>(AuraAttributeSet); 
-			MutableAAS->SetHealth(AuraAttributeSet->GetHealth() + 50.f); 
-			MutableAAS->SetMana(AuraAttributeSet->GetMana() - 25.f);
-			Destroy();
-		}
-
-	}
-}
-
-void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+	
 }
 
 void AAuraEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+
 }
+
+void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	TScriptInterface<IAbilitySystemInterface> ASCInterface = TScriptInterface<IAbilitySystemInterface>(Target);
+	if (ASCInterface)
+	{
+		UAbilitySystemComponent* TargetABSC = ASCInterface->GetAbilitySystemComponent();
+		//UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target); // 한번에 가져올 수도 있음.
+		if (TargetABSC == nullptr) return;
+
+		check(GameplayEffectClass);
+		FGameplayEffectContextHandle EffectContextHandle = TargetABSC->MakeEffectContext(); //FGameplayEffectContextHandle - FGameplayEffectContext를 포인터로 저장하는 Light Wrapper.
+		
+		EffectContextHandle.AddSourceObject(this); // Gameplay Context관련 항목을 저장하는 기능.  이 객체가 무슨 효과를 일으켰는지 SourceObject를 통해 확인 가능해짐.
+		
+		// SpecHandle이지만 보통 Spec으로 변수명을 지으니 유형 확인해볼 것, 똑같이 GameplayEffectSpec을 저장하는 LightWrapper임.
+		FGameplayEffectSpecHandle EffectSpecHandle = TargetABSC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle); 
+		TargetABSC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get()); // Data는 스마트포인터. Get()을 통해 raw Pointer로 가져옴 / 매개변수가 참조형식이라 값을 가져와서 넣어야 함(*EffectSpecHandle.Data.Get())
+	}
+}
+
+
+
 
 
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "AuraEffectActor.generated.h"
 
+class UGameplayEffect;
+
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
 {
@@ -12,20 +14,16 @@ class AURA_API AAuraEffectActor : public AActor
 public:
 	AAuraEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
 protected:
+	
 	virtual void BeginPlay() override;
+
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UPROPERTY(EditAnywhere, Category = "Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
 
 private:
 
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<class USphereComponent> Sphere;
-
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<class UStaticMeshComponent> Mesh;
 };
