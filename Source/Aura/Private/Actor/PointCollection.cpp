b6518d9cc14fc19b59cc91ab9fadf44af9f14561
chano062400 <sch062400@gmail.com>
2024-04-08 02:47:13 +0900

    Point Collection - 1

---
+++ Source/Aura/Private/Actor/PointCollection.cpp
@@ -0,0 +1,106 @@
+#include "Actor/PointCollection.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APointCollection::APointCollection()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Pt_0 = CreateDefaultSubobject<USceneComponent>("Pt_0");
+	ImmutablePoints.Add(Pt_0);
+	SetRootComponent(Pt_0);
+
+	Pt_1 = CreateDefaultSubobject<USceneComponent>("Pt_1");
+	ImmutablePoints.Add(Pt_1);
+	Pt_1->SetupAttachment(GetRootComponent());
+
+	Pt_2 = CreateDefaultSubobject<USceneComponent>("Pt_2");
+	ImmutablePoints.Add(Pt_2);
+	Pt_2->SetupAttachment(GetRootComponent());
+
+	Pt_3 = CreateDefaultSubobject<USceneComponent>("Pt_3");
+	ImmutablePoints.Add(Pt_3);
+	Pt_3->SetupAttachment(GetRootComponent());
+
+	Pt_4 = CreateDefaultSubobject<USceneComponent>("Pt_4");
+	ImmutablePoints.Add(Pt_4);
+	Pt_4->SetupAttachment(GetRootComponent());
+
+	Pt_5 = CreateDefaultSubobject<USceneComponent>("Pt_5");
+	ImmutablePoints.Add(Pt_5);
+	Pt_5->SetupAttachment(GetRootComponent());
+
+	Pt_6 = CreateDefaultSubobject<USceneComponent>("Pt_6");
+	ImmutablePoints.Add(Pt_6);
+	Pt_6->SetupAttachment(GetRootComponent());
+
+	Pt_7 = CreateDefaultSubobject<USceneComponent>("Pt_7");
+	ImmutablePoints.Add(Pt_7);
+	Pt_7->SetupAttachment(GetRootComponent());
+
+	Pt_8 = CreateDefaultSubobject<USceneComponent>("Pt_8");
+	ImmutablePoints.Add(Pt_8);
+	Pt_8->SetupAttachment(GetRootComponent());
+
+	Pt_9 = CreateDefaultSubobject<USceneComponent>("Pt_9");
+	ImmutablePoints.Add(Pt_9);
+	Pt_9->SetupAttachment(GetRootComponent());
+
+	Pt_10 = CreateDefaultSubobject<USceneComponent>("Pt_10");
+	ImmutablePoints.Add(Pt_10);
+	Pt_10->SetupAttachment(GetRootComponent());
+}
+
+TArray<USceneComponent*> APointCollection::GetGroundPoints(const FVector& GroundLocation, int32 NumOfPoints, float YawOverride)
+{
+	checkf(ImmutablePoints.Num() >= NumOfPoints, TEXT("Attempted to Access ImmutablePoints out of bounds."));
+
+	TArray<USceneComponent*> ArrayCopy;
+
+	for (USceneComponent* Point : ImmutablePoints)
+	{
+		if (ArrayCopy.Num() > NumOfPoints) return ArrayCopy;
+		
+		// Pt_0은 중심이기 때문에 회전할 필요가 없음.
+		if (Point != Pt_0)
+		{
+			FVector ToPoint = (Point->GetComponentLocation() - Pt_0->GetComponentLocation()).GetSafeNormal();
+			ToPoint = ToPoint.RotateAngleAxis(YawOverride, FVector::UpVector);
+			Point->SetWorldLocation(Pt_0->GetComponentLocation() + ToPoint);
+		}
+
+		// LineTrace 시작, 끝 점.
+		const FVector RaisedLocation = FVector(Point->GetComponentLocation().X, Point->GetComponentLocation().Y, Point->GetComponentLocation().Z + 500.f);
+		const FVector LoweredLocation = FVector(Point->GetComponentLocation().X, Point->GetComponentLocation().Y, Point->GetComponentLocation().Z - 500.f);
+
+		FHitResult HitResult;
+		// Player들을 무시하고 Spawn할 것이기 때문.
+		TArray<AActor*> IgnoreActors;
+		UAuraAbilitySystemLibrary::GetLivePlayersWithInRadius(this, IgnoreActors, TArray<AActor*>(), 1500, GetActorLocation());
+
+		FCollisionQueryParams Params;
+		Params.AddIgnoredActors(IgnoreActors);
+
+		GetWorld()->LineTraceSingleByProfile(
+			HitResult,
+			RaisedLocation,
+			LoweredLocation,
+			FName("BlockAll"),
+			Params
+		);
+
+		const FVector AdjustedLocation = FVector(Point->GetComponentLocation().X, Point->GetComponentLocation().Y, HitResult.ImpactPoint.Z);
+		Point->SetWorldLocation(AdjustedLocation);
+		Point->SetWorldRotation(UKismetMathLibrary::MakeRotFromZ(HitResult.ImpactNormal));
+
+		ArrayCopy.Add(Point);
+	}
+	
+	return ArrayCopy;
+}
+
+void APointCollection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
